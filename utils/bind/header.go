b81355493f9adcece31d9838a98f35eae9b8f9b4
Amir Mousavi <amirmousavi0003@gmail.com>
2024-04-06 09:26:31 +0330

    change bind functions parameters name

--- utils/bind/header.go
+++ utils/bind/header.go
@@ -2,9 +2,9 @@ package bind
 
 import "github.com/gin-gonic/gin"
 
-func BindHeader[T any](c *gin.Context) (*T, error) {
+func BindHeader[T any](ctx *gin.Context) (*T, error) {
 	param := new(T)
-	if err := c.BindHeader(param); err != nil {
+	if err := ctx.BindHeader(param); err != nil {
 		return nil, err
 	}
 	return param, nil
--- utils/bind/json.go
+++ utils/bind/json.go
@@ -2,9 +2,9 @@ package bind
 
 import "github.com/gin-gonic/gin"
 
-func BindJson[T any](c *gin.Context) (*T, error) {
+func BindJson[T any](ctx *gin.Context) (*T, error) {
 	param := new(T)
-	if err := c.BindJSON(param); err != nil {
+	if err := ctx.BindJSON(param); err != nil {
 		return nil, err
 	}
 	return param, nil
