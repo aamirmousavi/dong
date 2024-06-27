b08d82448b1da2b34dc7aec7358f27d0649e1a6d
Amir Mousavi <amirmousavi0003@gmail.com>
2024-06-27 14:18:01 +0330

    chore: Update CORS headers to allow all headers in internal/middleware/cors.go

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -7,7 +7,8 @@ func CORS() gin.HandlerFunc {
 		ctx.Writer.Header().Set("Content-Type", "application/json; charset=utf-8")
 		ctx.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		ctx.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		// ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With, Sec-Fetch-Dest, Sec-Fetch-Mode, Sec-Fetch-Site, User-Agent, Referer, Host, Connection, Accept-Language, Accept")
+		ctx.Writer.Header().Set("Access-Control-Allow-Headers", "*")
 		ctx.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
 
 		if ctx.Request.Method == "OPTIONS" {
