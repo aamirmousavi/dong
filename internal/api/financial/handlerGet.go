c3b9b2a21a88440c61edf859b912b8a6c0e654a6
Amir Mousavi <amirmousavi0003@gmail.com>
2024-06-27 16:53:05 +0330

    chore: Update financial API handlerGet.go to retrieve payments and factors for a user

--- internal/api/financial/handlerGet.go
+++ internal/api/financial/handlerGet.go
@@ -1,7 +1,34 @@
 package financial
 
-import "github.com/gin-gonic/gin"
+import (
+	interfaces_context "github.com/aamirmousavi/dong/interfaces/context"
+	interfaces_profile "github.com/aamirmousavi/dong/interfaces/profile"
+	"github.com/gin-gonic/gin"
+)
 
 func get(ctx *gin.Context) {
+	app := interfaces_context.GetAppContext(ctx)
+	profile := interfaces_profile.GetProfile(ctx)
+	payments, err := app.Mongo().BalanceHandler.PaymentList(
+		nil,
+		&profile.User.Id,
+	)
+	if err != nil {
+		ctx.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+
+	factors, err := app.Mongo().PeroidHandler.FactorListByUser(profile.User.Id)
+	if err != nil {
+		ctx.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.JSON(200, gin.H{
+		"total_demand": 1_000,
+		"total_debt":   2_000,
+		"payments":     payments,
+		"factors":      factors,
+	})
 
 }
